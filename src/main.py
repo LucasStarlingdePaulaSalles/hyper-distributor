911c22af8b799de4a9678bb8355c32de7c0b1b26
Lucas <lucas@inspr.com>
2021-06-14 16:24:19 -0300

    commenting code for improved understanding

--- src/main.py
+++ src/main.py
@@ -9,50 +9,72 @@ np.seterr(divide='ignore', invalid='ignore')
 help = "File format:\n<x-prior-lenght>,<y-output-lenght>\n<prior-array>\n<x-by-y-channel-matrix>"
 
 def main():
+    # parsing argument
     args = sys.argv[1:]
     if len(args) != 1:
         print("hyper-distributor expects a file path as argument: `hyper-distributor <file-path>")
         print(help)
         return
+    
+    # opening file
     file_name = args[0]
     file = open(file_name,"r")
-    x,y = [int(x) for x in file.readline().split(',')]
-    prior = [float(Fraction(x)) for x in file.readline().split(',')]
-    prior = np.array(prior, dtype=float)
-    prior.shape = (prior.size,1)
+
+    #reading problem instance:
+    #1-problem's size
+    x,y = [int(x) for x in file.readline().split(',')] 
     print(f"|X| = {x}")
     print(f"|Y| = {y}")
-    if sum(prior) != 1.0:
+
+    #2-problem's prior
+    prior = [float(Fraction(x)) for x in file.readline().split(',')] 
+    prior = np.array(prior, dtype=float)
+    prior.shape = (prior.size,1) #makes the prior a column vector instead of a simple array
+    if sum(prior) != 1.0: #validating that the prior configures a probability distribution
         print("prior distribution should sum to 1")
         return
     print(f"Prior distribution:\n{prior}\n")
-    cmatrix = np.ndarray(shape=(x,y),dtype=float)
+
+    #3-problem's channel matrix
+    cmatrix = np.ndarray(shape=(x,y),dtype=float) #allocating space according to declared size
     for i in range(x):
-        cmatrix[i] = [float(Fraction(x)) for x in file.readline().split(',')] 
-    partialdist = np.sum(a=cmatrix,axis=1)
-    print(f"Partial channel distributions:\n{partialdist}\n")
-    for i in range(x):
-        if 1.0 - partialdist[i] > 0.0001: # treshold a 1/100%
+        cmatrix[i] = [float(Fraction(x)) for x in file.readline().split(',')] #line by line matrix is read and separated by commas, Fractions allows user to write probabilities in 'n/d' notation 
+    partialdist = np.sum(a=cmatrix,axis=1)  #rows are added together resulting in an array with x elements
+    print(f"Partial channel distributions:\n{partialdist}\n") #each of these elements represents the sum of a probability distribuition of the outputs given an input
+    for i in range(x): # validating that all rows configure probability distribuitions
+        if abs(1.0 - partialdist[i]) > 0.0001: # treshold of 1/100%
             print(f"ERROR: every distribuition of y's for a given x shoud sum to 1, for x{i} it is:{partialdist[i]}\n")
             return
     print(f"Channel matrix:\n{cmatrix}\n")
+
+    #calculate joint probability matrix
     joint = cmatrix*prior
     print(f"Joint matrix:\n{joint}\n")
-    py = np.sum(joint,axis=0)
+
+    #from joint matrix calculate output probability distribution
+    py = np.sum(joint,axis=0) #summing columns result in an array of y elements representing the probability of each output happening
+    print(f"P[Y]:\n{py}\n")
+    
+    #cleaning problem by discarting outputs that have probability 0 and their respectives columns on the joint matrix
     joint = np.delete(joint,np.where(py == np.array([0.0])),1)
     py = py[np.where(py != np.array([0.0]))]
     py.shape = (py.size,1)
     print(f"P[Y]:\n{py}\n")
+    print(f"Joint matrix:\n{joint}\n")
+
+    #calculate postrior distribution matrix
     posterior = joint.T/py
     posterior = posterior.T
     print(f"Posterior distribuition(xi,PX|yi):\n{posterior}\n")
+
+    #calculate hyper distribuiton matrix by discarting non unique colums and adding their respective probabilities together
     hyper, unique, inverse = np.unique(posterior,
                                  return_index=True,
                                  return_inverse=True,
                                  axis=1)
     pyn = np.ndarray(shape=(unique.size,1))
     for i in unique:
-        if i < unique.size:
+        if i < unique.size: #this eliminates problems related to corner cases of the unique method from numpy library
             pyn[i] = sum(py[np.where(inverse == i)])
     print(f"PHyper[Y]:\n{pyn}\n")
     print(f"Hyper distribuition(xi,PX|yi):\n{hyper}\n")
