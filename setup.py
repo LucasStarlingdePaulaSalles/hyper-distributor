00d60dd9a39965de6e05772e96bd8fa2388862bf
Lucas <lucas@inspr.com>
2021-08-11 20:57:05 -0300

    dev: improvments for pip

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example_qifhdc",
+    name="qifhdc",
     version="0.0.1",
     author="Lucas Starling",
     author_email="lucastarling1@gmail.com",
